be28e7d7edbec22bf612e3eea34b9b139d92547c
ppyore <39377053+ppyore@users.noreply.github.com>
2018-06-08 00:16:57 +0800

    Add files via upload

---
+++ num2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace num2
+{
+    class num2
+    {
+        static void Main(string[] args)
+        {
+            int? i = 10;
+            if (i != null)
+            {
+                Console.WriteLine(i.Value);
+            }
+            else
+            {
+               Console.WriteLine("Undefined");
+            }
+        }
+    }
+}
