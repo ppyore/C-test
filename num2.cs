26a54bf39e22628962401f63ff73ffb8cdcb1dc9
ppyore <39377053+ppyore@users.noreply.github.com>
2018-06-07 23:52:29 +0800

    Add files via upload

---
+++ num2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class num2 : Form
+    {
+        public num2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int? i = 10;
+            double? d1 = 3.14;
+            bool? flag = null;
+            char? letter = 'a';
+            int?[] arr = new int?[10];
+            
+        }       
+    }
+}
