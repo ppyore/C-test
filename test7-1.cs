a960a5b72a003764b370081c31fd9bd11459b6aa
ppyore <39377053+ppyore@users.noreply.github.com>
2018-06-10 23:46:26 +0800

    Add files via upload

---
+++ test7-1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test7
+{
+    class QueryVMethodSyntax
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 10, 8, 3, 6, 12 };
+            //Query syntax:
+            IEnumerable<int> numQuery1 =
+                from num in numbers
+                where num % 2 == 0
+                orderby num
+                select num;
+            foreach (int i in numQuery1)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine(System.Environment.NewLine);
+            Console.ReadLine();
+        }
+    }
+}
