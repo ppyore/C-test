1304d3ab55eb5d161914363a1b903daf5739d113
ppyore <39377053+ppyore@users.noreply.github.com>
2018-06-10 21:56:42 +0800

    Add files via upload

---
+++ test2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test2
+{
+    class test2
+    {
+        public class myGeneric<T>
+        {
+            public void myTest(T myValue)
+            {
+                System.Type myType = myValue.GetType();
+                Console.WriteLine(myType);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int? i = 10;
+            if (i != null)
+            {
+                Console.WriteLine(i.Value);
+            }
+            else
+            {
+                Console.WriteLine("Undefined");
+            }
+            Console.ReadLine();
+        }
+    }
+}
