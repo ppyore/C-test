5ca26c7e25b4873284c56f7beb6bfad0077f6849
ppyore <39377053+ppyore@users.noreply.github.com>
2018-06-10 21:00:22 +0800

    Add files via upload

---
+++ test3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test3
+{
+    class test3
+    {    public class myGeneric<T>
+        {
+            //泛型方發宣告
+            public void myTest(T myValue)
+            {
+                System.Type myType = myValue.GetType();
+                Console.WriteLine(myType);
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<string> myGen = new List<string>();
+            myGen.Add("Amy");
+            myGen.Add("Barry");
+            myGen.Add("Allen");
+            myGen.Add("Clark");
+            myGen.Add("Bill");
+
+            foreach (string i in myGen)
+            {
+             Console.WriteLine(i);            
+            }
+             Console.ReadLine();
+        }        
+    }
+}
+
